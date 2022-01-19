e57e452607da8e501b205ad70e50f86fa701f701
st81 <m59rt@yahoo.co.jp>
2022-01-19 09:12:54 +0900

    Delete: Uneccessary codes

--- train.py
+++ train.py
@@ -1,9 +1,8 @@
-from argparse import ArgumentParser, Namespace
+from argparse import Namespace
 
 from dotenv import load_dotenv
 from pytorch_lightning import Trainer
 from pytorch_lightning.utilities.seed import seed_everything
-from pytorch_lightning import loggers as pl_loggers
 
 from datamodules.bases import MNISTDataModule
 from models.bases import MNISTModel
@@ -14,7 +13,6 @@ from utils.utils import prepare_callbacks, prepare_loggers
 def main(args: Namespace) -> None:
     datamodule = MNISTDataModule.from_argparse_args(args)
     model = MNISTModel(**args.__dict__)
-    # wandb_logger = pl_loggers.WandbLogger()
     trainer: Trainer = Trainer.from_argparse_args(
         args,
         callbacks=prepare_callbacks(args.is_swa),
